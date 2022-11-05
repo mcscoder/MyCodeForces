485da153c989ad3c023e591a1fb85be7f1b6c52d
mcscoder <113981131+mcscoder@users.noreply.github.com>
2022-11-05 10:41:02 +0700

    Create B - Interesting Sum.cpp

---
+++ B - Interesting Sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int t; cin >> t;
+    while (t--) {
+        int n; cin >> n;
+        int a[n]; for(int &i : a) cin >> i;
+        sort(a, a + n);
+        cout << a[n - 1] + a[n - 2] - a[0] - a[1] << endl;
+    }
+}
