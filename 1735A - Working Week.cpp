21e66f8014a8b9a9d229cdd7879f62075d1dffd5
mcscoder <113981131+mcscoder@users.noreply.github.com>
2022-11-09 11:46:34 +0700

    Create 1735A - Working Week.cpp

---
+++ 1735A - Working Week.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> // sort
+#include <map>
+#include <cmath>
+
+// #define int long long
+
+using namespace std;
+
+void sol();
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    sol();
+}
+
+void sol() {
+    int t; cin >> t;
+    while(t--) {
+        int n; cin >> n;
+        cout << (n - 3) / 3 - 1 << endl;
+    }
+}
